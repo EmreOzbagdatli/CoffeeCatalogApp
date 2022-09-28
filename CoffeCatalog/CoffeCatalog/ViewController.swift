efa36fea483db29b61679b8bb7afd8e73d0f871e
Emre Özbağdatlı <74432853+EmreOzbagdatli@users.noreply.github.com>
2022-09-28 16:41:39 +0300

    Creating TableView

--- CoffeCatalog/CoffeCatalog/ViewController.swift
+++ CoffeCatalog/CoffeCatalog/ViewController.swift
@@ -7,13 +7,31 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource{
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+
+        tableView.delegate = self
+        tableView.dataSource = self
+        
     }
 
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.imageView?.image = UIImage(named:"americano")
+        cell.textLabel?.text = "americano"
+        return cell
+    }
+    
+    
 }
 
