f0d3989e9ee55c79bca2b5fc4067ed27cebe985f
Mehmet Ali Demir <64283995+mehmetalidemir@users.noreply.github.com>
2022-09-29 13:53:55 +0300

    indentetion fix

--- CoffeCatalog/CoffeCatalog/ViewController.swift
+++ CoffeCatalog/CoffeCatalog/ViewController.swift
@@ -7,18 +7,15 @@
 
 import UIKit
 
-class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource{
+final class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     @IBOutlet weak var tableView: UITableView!
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        
 
         tableView.delegate = self
         tableView.dataSource = self
-        
     }
 
 
@@ -27,11 +24,9 @@ class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource
     }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell()
-        cell.imageView?.image = UIImage(named:"americano")
+        cell.imageView?.image = UIImage(named: "americano")
         cell.textLabel?.text = "americano"
         return cell
     }
-    
-    
 }
 
